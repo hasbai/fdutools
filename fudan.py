2022a1f007e4d54185b1057a5dc8c4e0f6cb8c60
shi <jsclndnz@gmail.com>
2021-06-30 12:33:35 +0800

    一些重构

--- fudan.py
+++ fudan.py
@@ -1,80 +1,90 @@
 from lxml import etree
 from bs4 import BeautifulSoup
+import httpx
+import config
 
 
-def login(c, uis_url, service_url, username, password):
-    r = c.get(uis_url, params={'service': service_url})
-    assert r.status_code == 200, '网络错误'
+class Fudan:
+    def __init__(self, username, password):
+        self.c = httpx.Client(headers={'User-Agent': config.ua})
 
-    html = etree.HTML(r.text, etree.HTMLParser())
-    data = {
-        'username': username,
-        'password': password,
-    }
-    data.update(zip(
-        html.xpath("/html/body/form/input/@name"),
-        html.xpath("/html/body/form/input/@value")
-    ))
+        self.username = username
+        self.password = password
 
-    r = c.post(
-        uis_url,
-        params={'service': service_url},
-        headers={
-            'Host':     'uis.fudan.edu.cn',
-            'Origin':   'https://uis.fudan.edu.cn',
-        },
-        data=data,
-        allow_redirects=False
-    )
+    def login(self, service_url):
+        r = self.c.get(config.uis_url, params={'service': service_url})
+        assert r.status_code == 200, '网络错误'
 
-    assert service_url + '?ticket' in r.headers['location'], '登录失败'
+        html = etree.HTML(r.text, etree.HTMLParser())
+        data = {
+            'username': self.username,
+            'password': self.password,
+        }
+        data.update(zip(
+            html.xpath("/html/body/form/input/@name"),
+            html.xpath("/html/body/form/input/@value")
+        ))
 
+        r = self.c.post(
+            config.uis_url,
+            params={'service': service_url},
+            headers={
+                'Host': 'uis.fudan.edu.cn',
+                'Origin': 'https://uis.fudan.edu.cn',
+            },
+            data=data,
+            allow_redirects=False
+        )
 
-def logout(c, logout_url, login_url):
-    r = c.get(logout_url)
-    response_url = str(r.url)
-    assert login_url in response_url, '{} is not in {}'.format(login_url, response_url)
+        assert service_url + '?ticket' in r.headers['location'], '登录失败'
 
+    def close(self, logout_url):
+        r = self.c.get(logout_url)
+        self.c.close()
+        # response_url = str(r.url)
+        # assert config.uis_url in response_url, '{} is not in {}'.format(config.uis_url, response_url)
 
-def get_grade(c, semester_id):
-    r = c.get('https://jwfw.fudan.edu.cn/eams/teach/grade/course/person!search.action',
-              params={'semesterId': semester_id})
-    if '当前用户存在重复登录的情况' in r.text:
+    # def close(self):
+    #     self.c.close()
+
+    def get_grade(self, semester_id):
+        r = self.c.get('https://jwfw.fudan.edu.cn/eams/teach/grade/course/person!search.action',
+                       params={'semesterId': semester_id})
+        if '当前用户存在重复登录的情况' in r.text:
+            soup = BeautifulSoup(r.text, features='lxml')
+            r = self.c.get(soup.a['href'], params={'semesterId': semester_id})
+
         soup = BeautifulSoup(r.text, features='lxml')
-        r = c.get(soup.a['href'], params={'semesterId': semester_id})
+        result = []
+        for item in soup.tbody.children:
+            strings = list(item.stripped_strings)
+            result.append(strings[3] + ' ' + strings[6])
+        return result
 
-    soup = BeautifulSoup(r.text, features='lxml')
-    result = []
-    for item in soup.tbody.children:
-        strings = list(item.stripped_strings)
-        result.append(strings[3] + ' ' + strings[6])
-    return result
+    def get_gpa(self):
+        r = self.c.get('https://jwfw.fudan.edu.cn/eams/myActualGpa!search.action')
+        if '当前用户存在重复登录的情况' in r.text:
+            soup = BeautifulSoup(r.text, features='lxml')
+            r = self.c.get(soup.a['href'])
 
-
-def get_gpa(c):
-    r = c.get('https://jwfw.fudan.edu.cn/eams/myActualGpa!search.action')
-    if '当前用户存在重复登录的情况' in r.text:
         soup = BeautifulSoup(r.text, features='lxml')
-        r = c.get(soup.a['href'])
+        gpas = []
+        major = ''
+        gpa = ''
 
-    soup = BeautifulSoup(r.text, features='lxml')
-    gpas = []
-    major = ''
-    gpa = ''
+        for item in soup.tbody.children:
+            strings = list(item.stripped_strings)
+            if strings[0][0] != '*':
+                major = strings[3]
+                gpa = strings[5]
+                break
 
-    for item in soup.tbody.children:
-        strings = list(item.stripped_strings)
-        if strings[0][0] != '*':
-            major = strings[3]
-            gpa = strings[5]
-            break
+        for item in soup.tbody.children:
+            strings = list(item.stripped_strings)
+            if strings[3] == major:
+                gpas.append(strings[5])
 
-    for item in soup.tbody.children:
-        strings = list(item.stripped_strings)
-        if strings[3] == major:
-            gpas.append(strings[5])
-
-    gpas.sort()
-    gpas.reverse()
-    percentage = (gpas.index(gpa) + 1) / len(gpas) * 100
-    return '我的绩点为：{}\n专业排名为：{:.1f}%'.format(gpa, percentage)
+        gpas.sort()
+        gpas.reverse()
+        percentage = (gpas.index(gpa) + 1) / len(gpas) * 100
+        return '我的绩点为：{}\n专业排名为：{:.1f}%'.format(gpa, percentage)
--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-import httpx
-
 import config
 import utils
-from fudan import *
+from fudan import Fudan
 
-if __name__ == '__main__':
 
-    with httpx.Client(headers={'User-Agent': config.ua}) as c:
-        login(c, config.uis_url, config.jwfw_url, config.username, config.password)
-        grades = get_grade(c, config.semester_id)
-        gpa = get_gpa(c)
-        logout(c, config.jwfw_logout_url, config.uis_url)
+def grade_report():
+    try:
+        c = Fudan(config.username, config.password)
+        c.login(config.jwfw_url)
+        grades = c.get_grade(config.semester_id)
+        gpa_report = c.get_gpa()
+    except Exception as e:
+        print(e)
+    finally:
+        c.close(config.jwfw_logout_url)
 
-    with open('result.txt', 'r') as file:
-        grades_num = int(file.readline())
+    try:
+        with open('result.txt', 'r') as file:
+            grades_num = int(file.readline())
+    except (ValueError, FileNotFoundError):
+        with open('result.txt', 'w') as file:
+            file.write('0')
+        grades_num = 0
 
     if grades_num != len(grades):
-        print(grades)
-        print(gpa)
+        [print(grade) for grade in grades]
+        print(gpa_report)
 
         content = ''
         for s in grades:
             content = content + s + '\r\n'
-        content = content + gpa
+        content = content + gpa_report
         utils.send_email('考试成绩快报', content)
 
         with open('result.txt', 'w') as file:
             file.write(str(len(grades)))
+
+
+if __name__ == '__main__':
+    grade_report()
