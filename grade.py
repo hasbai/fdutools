f7a9bd8d8914286d6f105435b7ffb89b77ccd374
hasbai <jsclndnz@outlook.com>
2022-01-19 16:58:38 +0800

    update

--- grade.py
+++ grade.py
@@ -1,5 +1,6 @@
 import dbm
 
+import pandas as pd
 from bs4 import BeautifulSoup
 
 import config
@@ -16,38 +17,48 @@ class Grade(Fudan):
             self.jwfw = True
         except:
             self.jwfw = False
+        self.full_grades = None
+
+    def get_full_grades(self):
+        if self.full_grades is not None:
+            return self.full_grades
+        grade_url = 'https://my.fudan.edu.cn/list/bks_xx_cj'
+        r = self.c.get(grade_url, follow_redirects=True)
+        soup = BeautifulSoup(r.text, features='lxml')
+        df = pd.DataFrame(
+            data=[tr.stripped_strings for tr in soup.tbody.children],
+            columns=['code', 'year', 'semester', 'name', 'credit', 'grade']
+        )  # ['课程代码', '学年', '学期', '课程名称', '学分', '最终成绩']
+        df.credit = df.credit.astype('float')
+        df = df[df.grade != 'P']  # 过滤 P 的课程
+        # 转换绩点
+        trans = {
+            'A': 4.0,
+            'A-': 3.7,
+            'B+': 3.3,
+            'B': 3.0,
+            'B-': 2.7,
+            'C': 2.4
+        }
+        point = []
+        for i in df['grade']:
+            point.append(trans.get(i, 2.0))
+        df['point'] = point
+
+        self.full_grades = df
+        return df
 
     def get_grade(self):
-        result = []
-
-        if self.jwfw:  # 教务网站通道
-            grade_url = 'https://jwfw.fudan.edu.cn/eams/teach/grade/course/person!search.action'
-            r = self.c.get(grade_url, params={'semesterId': self.semester_id}, follow_redirects=True)
-            soup = BeautifulSoup(r.text, features='lxml')
-            for tr in soup.tbody.children:
-                li = list(tr.stripped_strings)
-                if len(li) > 6:
-                    result.append(li[3] + ' ' + li[6])
-
-        else:  # my.fudan.edu.cn 通道
-            grade_url = 'https://my.fudan.edu.cn/list/bks_xx_cj'
-            r = self.c.get(grade_url, follow_redirects=True)
-            soup = BeautifulSoup(r.text, features='lxml')
-            year, semester = None, None
-            for tr in soup.tbody.children:
-                li = list(tr.stripped_strings)
-                if year is None:
-                    year = li[1]
-                    semester = li[2]
-                if year != li[1] or semester != li[2]:
-                    break
-                result.append(li[3] + ' ' + li[5])
-
-        return result
+        df = self.get_full_grades()
+        year, semester = df.year[0], df.semester[0]
+        df = df[(df.year == year) & (df.semester == semester)]
+        return list(map(lambda i: f'{i[0]}  {i[1]}', zip(df.name, df.grade)))
 
     def get_gpa(self):
         if not self.jwfw:
-            return '教务网站不可用，无法查询绩点！'
+            df = self.get_full_grades()
+            gpa = sum(df.credit * df.point) / sum(df.credit)
+            return f'我的绩点为：{gpa:.2f}\n教务网站不可用，无法查询排名！'
 
         gpa_url = 'https://jwfw.fudan.edu.cn/eams/myActualGpa!search.action'
         r = self.c.get(gpa_url, follow_redirects=True)
@@ -72,7 +83,7 @@ class Grade(Fudan):
         gpas.sort()
         gpas.reverse()
         percentage = (gpas.index(gpa) + 1) / len(gpas) * 100
-        return '我的绩点为：{}\n专业排名为：{:.1f}%'.format(gpa, percentage)
+        return f'我的绩点为：{gpa:.2f}\n专业排名为：{percentage:.1f}%'
 
 
 def grade_report(username, password, email, is_send_email=True, **kwargs):
@@ -97,3 +108,5 @@ def grade_report(username, password, email, is_send_email=True, **kwargs):
 
 if __name__ == '__main__':
     grade_report(**config.users[0])
+    # with Grade(**config.users[0]) as g:
+    #     print(g.get_gpa())
